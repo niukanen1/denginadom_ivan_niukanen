8d48e1fac915f6480d1e9c1c02b937d2d4a922fe
Ivan Niukanen <ivanniu2003@gmail.com>
2023-07-14 19:47:52 +0300

    Initial commit

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLayout name="default" >
+        <NuxtPage />
+    </NuxtLayout>
+   
+</template>
+
+<script setup>
+import '@/assets/css/main.css'
+</script>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-row px-3 py-2 bg-red justify-evenly ">
+        <NuxtLink to="/">Home</NuxtLink>
+    </div>
+    <slot />
+</template>
