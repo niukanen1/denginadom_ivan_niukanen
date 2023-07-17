4ec55234430c7fb486b4ff2e0bc8679c40312639
Ivan Niukanen <ivanniu2003@gmail.com>
2023-07-17 03:36:51 +0300

    MiniCharacterCard UI

---
+++ components/MiniCharacterCard/MiniCharacterCard.vue
@@ -0,0 +1,16 @@
+
+<script setup lang="ts">
+const { character }=defineProps<{
+    character: character
+}>()
+
+</script>
+
+<template>
+    <div class="flex flex-col p-2 border-[0.5px] rounded-md">
+        <NuxtLink :to="`/character:${character.id}`">
+            <img :src="character.image" />
+            <p class="text-lg font-bold">{{character.name}}</p>
+        </NuxtLink>
+    </div>
+</template>
